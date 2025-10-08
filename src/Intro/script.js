03afebaf44ef51b425e828ea5c94635df7325b5e
Bruno Norton <brunonorton30@gmail.com>
2025-10-08 17:35:48 -0300

    aula 08/10/2024 - React - Vite

---
+++ src/Intro/script.js
@@ -0,0 +1,15 @@
+   ReactDOM.render(
+    React.createElement(
+        'h1',
+        null,
+        'Hello, World!'
+    ),
+    document.getElementById('root'))
+
+   ReactDOM.render(
+    React.createElement(
+        'button',
+        null,
+        'Minhaperta'
+    ),
+    document.getElementById('buto'))
---
+++ src/my-react-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Header React App</h1>
+    </header>
+  );
+}
+
+export default Header;
